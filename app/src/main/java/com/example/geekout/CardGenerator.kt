b3a100eff3f6ee910e4da042b20330ffd5029121
Daniel Ben-Or <dbenor@terpmail.umd.edu>
2022-05-07 03:16:39 -0400

    Created card color methods

--- app/src/main/java/com/example/geekout/CardGenerator.kt
+++ app/src/main/java/com/example/geekout/CardGenerator.kt
@@ -6,12 +6,32 @@ class CardGenerator {
     public fun generateCard(color : Game.Roll) : Card {
 
         return when (color) {
-            Game.Roll.YELLOW -> Card(4, "Hello, this is a yellow card")
-            Game.Roll.RED -> Card(3, "Hello, this is a red card")
-            Game.Roll.BLUE -> Card(2, "Hello, this is a blue card")
-            Game.Roll.GREEN -> Card(5, "Hello, this is a green card")
-            Game.Roll.BLACK -> Card(6, "Hello, this is a black card")
+            Game.Roll.YELLOW -> generateYellow()
+            Game.Roll.RED -> generateRed()
+            Game.Roll.BLUE -> generateBlue()
+            Game.Roll.GREEN -> generateGreen()
+            Game.Roll.BLACK -> generateBlack()
         }
+    }
+
+    private fun generateYellow() : Card {
+        return Card(4, "Hello, this is a yellow card")
+    }
+
+    private fun generateRed() : Card {
+        return Card(3, "Hello, this is a red card")
+    }
+
+    private fun generateBlue() : Card {
+        return Card(2, "Hello, this is a blue card")
+    }
+
+    private fun generateGreen() : Card {
+        return Card(5, "Hello, this is a green card")
+    }
+
+    private fun generateBlack() : Card {
+        return Card(6, "Hello, this is a black card")
     }
 
 }
