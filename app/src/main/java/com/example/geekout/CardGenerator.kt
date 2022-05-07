7f0eef437d4fa9348ac8ccdf6141316c421ff02f
Daniel Ben-Or <dbenor@terpmail.umd.edu>
2022-05-07 03:48:24 -0400

    Added category labels for reference

--- app/src/main/java/com/example/geekout/CardGenerator.kt
+++ app/src/main/java/com/example/geekout/CardGenerator.kt
@@ -15,22 +15,27 @@ class CardGenerator {
     }
 
     private fun generateYellow() : Card {
+        // Sci-Fi
         return Card(4, "Hello, this is a yellow card")
     }
 
     private fun generateRed() : Card {
+        // Games
         return Card(3, "Hello, this is a red card")
     }
 
     private fun generateBlue() : Card {
+        // Comics
         return Card(2, "Hello, this is a blue card")
     }
 
     private fun generateGreen() : Card {
+        // Fantasy
         return Card(5, "Hello, this is a green card")
     }
 
     private fun generateBlack() : Card {
+        // Misc.
         return Card(6, "Hello, this is a black card")
     }
 
