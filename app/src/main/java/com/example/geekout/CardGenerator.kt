9dacfc5a5da435a44c35fa93aaf34ff6ca465d81
Daniel Ben-Or <dbenor@terpmail.umd.edu>
2022-05-07 03:12:58 -0400

    Updated card generator to take a category color parameter from Game

--- app/src/main/java/com/example/geekout/CardGenerator.kt
+++ app/src/main/java/com/example/geekout/CardGenerator.kt
@@ -3,8 +3,15 @@ package com.example.geekout
 class CardGenerator {
     // TODO Add libraries and actual card generation
 
-    public fun generateCard() : Card {
-        return Card(4, "Hello, this is a new card")
+    public fun generateCard(color : Game.Roll) : Card {
+
+        return when (color) {
+            Game.Roll.YELLOW -> Card(4, "Hello, this is a yellow card")
+            Game.Roll.RED -> Card(3, "Hello, this is a red card")
+            Game.Roll.BLUE -> Card(2, "Hello, this is a blue card")
+            Game.Roll.GREEN -> Card(5, "Hello, this is a green card")
+            Game.Roll.BLACK -> Card(6, "Hello, this is a black card")
+        }
     }
 
 }
