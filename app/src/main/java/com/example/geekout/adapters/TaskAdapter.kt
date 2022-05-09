3189d4c7fcc5e33c53aa1111124b86d7d924b64f
Daniel Ben-Or <dbenor@terpmail.umd.edu>
2022-05-08 20:29:05 -0400

    Updated xml file for task_fragment, set up recyclerview

--- app/src/main/java/com/example/geekout/adapters/TaskAdapter.kt
+++ app/src/main/java/com/example/geekout/adapters/TaskAdapter.kt
@@ -6,6 +6,7 @@ import android.os.Looper
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.EditText
 import android.widget.TextView
 import androidx.recyclerview.widget.RecyclerView
 import com.example.geekout.R
@@ -41,7 +42,8 @@ class TaskAdapter(private val context: Context):
 
     class ViewHolder internal constructor(view: View): RecyclerView.ViewHolder(view) {
         var responseView: View = view
-        var responseText: TextView? = null
+        var indexTextView: TextView? = null
+        var responseEditText: EditText? = null
     }
 
     override fun getItemViewType(position: Int): Int {
@@ -60,15 +62,16 @@ class TaskAdapter(private val context: Context):
         val v = LayoutInflater.from(parent.context).inflate(PLAYER_TYPE, parent, false)
         val viewHolder = ViewHolder(v)
 
-        viewHolder.responseView = v.findViewById(R.id.input_item)
+        viewHolder.indexTextView = v.findViewById(R.id.numberTextView)
+        viewHolder.responseEditText = v.findViewById(R.id.inputEditText)
 
         return viewHolder
     }
 
     override fun onBindViewHolder(holder: ViewHolder, position: Int) {
-        val player = mItems[position]
-//        holder.responseView.text = player
-//        holder.nameTextView?.text = player.getName()
-//        holder.avatarTextView?.text = player.getAvatar()
+        val spot = mItems[position]
+
+        holder.indexTextView?.text = (position + 1).toString() + "."
+        holder.responseEditText?.setText(spot)
     }
 }
