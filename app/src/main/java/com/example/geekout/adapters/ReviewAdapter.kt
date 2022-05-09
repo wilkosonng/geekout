90985be351f5717fb1457a40c1c94b94b5e435e8
Daniel Ben-Or <dbenor@terpmail.umd.edu>
2022-05-08 23:13:54 -0400

    Set up recyclerview for reviewing submissions

---
+++ app/src/main/java/com/example/geekout/adapters/ReviewAdapter.kt
@@ -0,0 +1,72 @@
+package com.example.geekout.adapters
+
+import android.content.Context
+import android.os.Handler
+import android.os.Looper
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import android.widget.TextView
+import androidx.recyclerview.widget.RecyclerView
+import com.example.geekout.R
+
+class ReviewAdapter(private val context: Context) :
+    RecyclerView.Adapter<ReviewAdapter.ViewHolder>() {
+
+    companion object {
+        private const val TAG = "REVIEW ADAPTER"
+        private const val PLAYER_TYPE = R.layout.player_score
+    }
+
+    private var mItems = ArrayList<String>()
+
+    fun clear() {
+        mItems.clear()
+        Handler(Looper.getMainLooper()).post {
+            notifyDataSetChanged()
+        }
+    }
+
+    fun set(responseList: ArrayList<String>) {
+        mItems = responseList
+        Handler(Looper.getMainLooper()).post {
+            notifyDataSetChanged()
+        }
+    }
+
+    fun getItem(pos: Int): Any {
+        return mItems[pos]
+    }
+
+    class ViewHolder internal constructor(view: View): RecyclerView.ViewHolder(view) {
+        var responseView: View = view
+        var reviewIndexTextView: TextView? = null
+        var responseTextView: TextView? = null
+    }
+
+    override fun getItemId(position: Int): Long {
+        return position.toLong()
+    }
+
+    override fun getItemCount(): Int {
+        return mItems.size
+    }
+
+    override fun onCreateViewHolder(parent: ViewGroup, viewType: Int): ReviewAdapter.ViewHolder {
+        val v = LayoutInflater.from(parent.context).inflate(PLAYER_TYPE, parent, false)
+        val viewHolder = ViewHolder(v)
+
+        viewHolder.reviewIndexTextView = v.findViewById(R.id.reviewNumber)
+        viewHolder.responseTextView = v.findViewById(R.id.reviewText)
+
+        return viewHolder
+    }
+
+    override fun onBindViewHolder(holder: ViewHolder, position: Int) {
+        val spot = mItems[position]
+
+        holder.reviewIndexTextView?.text = (position + 1).toString() + "."
+        holder.responseTextView?.text = spot
+    }
+
+}
