2cd9d3db0eefccf4b79280d934ea9cc200afa11b
Wilson Kong <wilkosonng@gmail.com>
2022-05-10 04:22:54 -0400

    Fixed everything!

--- app/src/main/java/com/example/geekout/activities/SettingsActivity.kt
+++ app/src/main/java/com/example/geekout/activities/SettingsActivity.kt
@@ -2,17 +2,25 @@ package com.example.geekout.activities
 
 import android.os.Bundle
 import android.app.Activity
+import android.content.Context
 import android.content.SharedPreferences
+import android.util.Log
 import android.widget.Button
 import com.example.geekout.R
 import android.widget.EditText
 import android.widget.Toast
 import androidx.preference.PreferenceManager
+import java.io.BufferedWriter
+import java.io.FileNotFoundException
+import java.io.OutputStreamWriter
 
 class SettingsActivity : Activity() {
     // Todo: Create Settings Screen
     companion object {
         private const val UN_KEY = "username"
+        private const val UN_FILE = "UN_File.txt"
+
+        private const val TAG = "SETTINGS"
     }
 
     private lateinit var mPrefs: SharedPreferences
@@ -35,11 +43,23 @@ class SettingsActivity : Activity() {
             val text = editText.text.toString()
 
             if (text.length in 2..12) {
+                if (!getFileStreamPath(UN_FILE).exists()) {
+                    try {
+                        val fos = openFileOutput(UN_FILE, Context.MODE_PRIVATE)
+                        val mWriter = BufferedWriter(OutputStreamWriter(fos))
+
+                        mWriter.write(text)
+                        mWriter.close()
+                    } catch (e: FileNotFoundException) {
+                        Log.i(TAG, "Error writing to file")
+                    }
+                }
+
                 mPrefs.edit()
                     .putString(UN_KEY, text)
                     .commit()
 
-                Toast.makeText(this, text, Toast.LENGTH_SHORT).show()
+                Toast.makeText(this, "Username set to $text", Toast.LENGTH_SHORT).show()
             } else {
                 Toast.makeText(
                     this,
